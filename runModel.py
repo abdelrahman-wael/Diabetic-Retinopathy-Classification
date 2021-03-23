b37b24d60e465c682de1c9b325a5ef0dc5159f76
Abdelrahman Wael <abdel.rahman.wael1@gmail.com>
2021-03-23 23:48:07 +0200

    updated training loop added metrics

--- runModel.py
+++ runModel.py
@@ -1,29 +1,54 @@
-def runModel(model , optimizer , dataloader   , device ,train):
+from sklearn.metrics import accuracy_score, confusion_matrix, cohen_kappa_score
 
+
+def runModel(model , optimizer , dataloader   , device ,train):
   if train:
     model.train()
+    mode = "training"
   else:
     model.eval()
+    mode = "validation"
 
-  lossfn = nn.CrossEntropyLoss() 
+  lossfn = nn.CrossEntropyLoss()
+  # check if gpu is available  
 
-  epochPreds = []
-  epochGT = []
+  # Initialize the prediction and label lists(tensors)
+  preds_list=torch.zeros(0,dtype=torch.long, device='cpu')
+  labels_list=torch.zeros(0,dtype=torch.long, device='cpu')
+  totalLoss = 0
 
+  model.to(device)
   with tqdm(dataloader, unit="batch" , position=0 , leave=True) as tepoch:
     for batch in tepoch:
+      # print(len(batch[0].keys()))
       images = batch["image"]
       labels =batch["dr_grade"]
-      images = images.to(device)
+      images = images.to(device,dtype=torch.float)
       labels = labels.to(device)
 
       preds=model(images)
       loss = lossfn(preds, labels)
       
+      totalLoss += loss.item()
+      loss.to(device)
       if train:
         loss.backward()
         optimizer.step()
+      
+      probabilties,preds =torch.max(preds , 1)
 
-      tepoch.set_postfix(loss=loss.item())
+      preds_list=torch.cat([preds_list, preds.view(-1).cpu()])
+      labels_list=torch.cat([labels_list, labels.view(-1).cpu()])
 
-  return pred,gt
+      del preds , labels
+
+      preds_numpy = preds_list.numpy()
+      labels_numpy = labels_list.numpy()
+      
+      currentKappa = cohen_kappa_score(labels_numpy, preds_numpy, weights='quadratic')
+      currentAccuracy = accuracy_score(labels_numpy, preds_numpy)
+  
+
+      tepoch.set_postfix(loss=totalLoss/len(preds_numpy), mode = mode +" accuracy ={}".format(currentAccuracy) ,kappa =  currentKappa)
+
+  return preds_numpy,labels_numpy
